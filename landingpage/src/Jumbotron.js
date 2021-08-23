7e805c576329c6c3efb4a948ee747cc62601df3d
Constanzal <conii.lopez@gmail.com>
2021-08-23 01:44:36 +0000

    formateo

--- landingpage/src/Jumbotron.js
+++ landingpage/src/Jumbotron.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 const Jumbotron = () => {
-    return <div className="jumbotron">
-        <h1 className="display-4">A Warm Welcome!</h1>
-        <p className="lead">Lorem ipsum dolor sit amet consectetur adipiscing elit, lobortis potenti fusce consequat imperdiet dignissim metus semper, tincidunt orci euismod porta maecenas ullamcorper. Integer aliquam urna porttitor condimentum consequat litora orci donec nascetur maecenas interdum lacus mi nisi rhoncus curabitur class, mattis erat bibendum fringilla ultrices mollis potenti ornare pharetra fermentum sollicitudin habitasse per scelerisque purus ut.</p>
-        <hr className="my-3"/>
+    return (
+        <div className="jumbotron">
+            <h1 className="display-4">A Warm Welcome!</h1>
+            <p className="lead">Lorem ipsum dolor sit amet consectetur adipiscing elit, lobortis potenti fusce consequat imperdiet dignissim metus semper, tincidunt orci euismod porta maecenas ullamcorper. Integer aliquam urna porttitor condimentum consequat litora orci donec nascetur maecenas interdum lacus mi nisi rhoncus curabitur class, mattis erat bibendum fringilla ultrices mollis potenti ornare pharetra fermentum sollicitudin habitasse per scelerisque purus ut.</p>
+            <hr className="my-3" />
             <a className="btn btn-primary btn-lg" href="#" role="button">Call to action!</a>
-            </div>
+        </div>
+    )
 }
 
 export default Jumbotron
