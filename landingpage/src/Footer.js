c3a783a529274a4b4c2b7e02db0b0fb588729542
Constanzal <conii.lopez@gmail.com>
2021-08-23 01:00:07 +0000

    navbar arreglada, falta footer y cards

--- landingpage/src/Footer.js
+++ landingpage/src/Footer.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-    return <nav className="navbar navbar-dark bg-dark text-center">
-    <p className="navbar-brand" style={{textAlign: "center"}}>Copyright © Your Website 2018</p>
-  </nav>
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+        <p className="navbar-brand">Copyright © Your Website 2018</p>
+    </nav>
+  )
 }
 
 export default Footer
