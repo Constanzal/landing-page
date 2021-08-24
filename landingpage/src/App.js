ba1d3db0ea46eec4107c5f57c0bf8341c21845be
Constanzal <conii.lopez@gmail.com>
2021-08-24 03:11:59 +0000

    imagenes

--- landingpage/src/App.js
+++ landingpage/src/App.js
@@ -12,10 +12,10 @@ function App() {
         <Navbar/>
         <Jumbotron/>
         <div className="cards">
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
+            <Card imagen="https://media.istockphoto.com/photos/dramatic-twilight-cloudscape-sunset-sunrise-picture-id1158514405?k=6&m=1158514405&s=612x612&w=0&h=Kyo1MLQeLP-cmyVB_ucvzVL17-iKapYnlnRdrs9NV-M="/>
+            <Card imagen="https://tr-images.condecdn.net/image/LDM0pgM40l1/crop/2040/f/gettyimages-1146431497.jpg"/>
+            <Card imagen="https://www.seozoom.co.uk/wp-content/uploads/2021/02/caroselli-immagine.jpg"/>
+            <Card imagen="https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2021/01/elefantes-2205783.jpg?itok=Kk1xc3mP"/>
         </div>
         <Footer/>
       </header>
--- landingpage/src/Card.js
+++ landingpage/src/Card.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Card = () => {
+const Card = (props) => {
   return (
       <div className="card text-center" style={{ width: "18rem", margin: "auto" }}>
-        <img src="https://media.istockphoto.com/photos/dramatic-twilight-cloudscape-sunset-sunrise-picture-id1158514405?k=6&m=1158514405&s=612x612&w=0&h=Kyo1MLQeLP-cmyVB_ucvzVL17-iKapYnlnRdrs9NV-M=" className="card-img-top" alt="..." />
+        <img src={props.imagen} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
           <p className="card-text">Lorem ipsum dolor sit amet consectetur adipiscing elit, lobortis potenti fusce consequat imperdiet dignissim metus semper, tincidunt orci euismod porta maecenas ullamcorper.</p>
